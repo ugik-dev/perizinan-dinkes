7576a6dea898646b64ecd57934eada0b0cc2819d
Ugik Dev <ugik.dev@gmail.com>
2023-02-19 00:16:37 +0700

    k

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
+ob_start();
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
